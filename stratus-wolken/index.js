13e8a65052320199b08488714859a26ec9a52afd
Lynnadam <l.adam@hbk-bs.de>
2025-05-19 00:40:04 +0200

    cirrus

--- stratus-wolken/index.js
+++ stratus-wolken/index.js
@@ -4,33 +4,36 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Cloud {
+class StratusCloud {
   constructor() {
+
     this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height * 0.6;
-    this.size = Math.random() * 80 + 50;
-    this.speed = Math.random() * 0.5 + 0.2;
+    this.y = Math.random() * canvas.height * 0.3 + 0.2 * canvas.height;
+    this.width = Math.random() * 300 + 500; 
+    this.height = Math.random() * 50 + 30; 
+    this.speed = Math.random() * 0.2 + 0.1;
+    this.color = "rgba(169, 169, 169, 0.8)"; 
   }
 
   draw() {
     ctx.beginPath();
-    ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.ellipse(this.x, this.y, this.width / 2, this.height / 2, 0, 0, Math.PI * 2);
     ctx.fill();
   }
 
   update() {
     this.x += this.speed;
-    if (this.x - this.size > canvas.width) {
-      this.x = -this.size;
-      this.y = Math.random() * canvas.height * 0.6;
+    if (this.x - this.width / 2 > canvas.width) {
+      this.x = -this.width / 2; 
+      this.y = Math.random() * canvas.height * 0.3 + 0.2 * canvas.height;
     }
   }
 }
 
 let clouds = [];
-for (let i = 0; i < 30; i++) {
-  clouds.push(new Cloud());
+for (let i = 0; i < 10; i++) {
+  clouds.push(new StratusCloud());
 }
 
 function animate() {
